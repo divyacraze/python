76dfec0a9972700ac51da244e876e26b9cfad90f
divyacraze <60418545+divyacraze@users.noreply.github.com>
2020-01-29 10:57:43 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,16 @@
+def add(a=3,b=9):
+ sum=a+b
+ print(sum)
+add(3,9)
+def sub(a=3,b=9):
+ sum=a-b
+ print(sum)
+sub(3,9)
+def mul(a=3,b=9):
+ sum=a*b        
+ print(sum)
+mul(3,9) 
+def div(a=3,b=9):
+ sum=a/b
+ print(sum)
+div(3,9)
